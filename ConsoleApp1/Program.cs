d39a18875b0294fd62c392b447ff7947e93502b3
Peťo <petermacus134@gmail.com>
2024-10-24 19:04:45 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rok = 2024;
+            Console.WriteLine("Npaíš rok narodenia");
+            int narodenia = int.Parse(Console.ReadLine());
+            int vek = rok - narodenia;
+            Console.WriteLine("Máš " + vek + " rokov");
+
+        }
+    }
+}
